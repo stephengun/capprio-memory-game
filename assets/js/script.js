23f30d5a19959583e3fec443daa8f59802efcf50
Paulina Hetman <info@pehaa.com>
2017-08-21 17:25:31 +0200

    initial init

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var Images = [
+  "assets/images/1.jpg", 
+  "assets/images/2.jpg", 
+  "assets/images/3.jpg", 
+  "assets/images/4.jpg", 
+  "assets/images/5.jpg", 
+  "assets/images/6.jpg", 
+  "assets/images/7.jpg", 
+  "assets/images/8.jpg",
+  "assets/images/9.jpg", 
+  "assets/images/10.jpg",
+  "assets/images/guts.png",
+  "assets/images/panda.jpg",
+];
